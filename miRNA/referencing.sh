4781f05ca0d9155f9f76918f5b7b25cf976f0d56
Radwa1982 <radwa_noreen@hotmail.co.uk>
2025-09-06 15:33:11 +0100

    Update referencing.sh

--- miRNA/referencing.sh
+++ miRNA/referencing.sh
@@ -5,3 +5,6 @@
 module avail
 module load
  bowtie-build Homo_sapiens.GRCh38.ncrna.fa.gz hg38_ncrna_index 
+# The mapping step 
+mapper.pl config.txt -d -j -h -e -l 18 -m -p ref/hg38_genome_index -s reads_1.fa -t reads_vs_genome_1.arf  
+#
