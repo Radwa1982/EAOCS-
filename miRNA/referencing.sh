efd13c2d048cfc66bc535cbaf343b76ca07f551f
Radwa1982 <radwa_noreen@hotmail.co.uk>
2025-09-12 21:02:44 +0100

    Update referencing.sh

--- miRNA/referencing.sh
+++ miRNA/referencing.sh
@@ -1,12 +1,3 @@
-# refrencing 
-   
-
-#Indexing using bowtie
-module avail
-module load
- bowtie-build 
-# The mapping step 
-mapper.pl config.txt -d -j -h -e -l 18 -m -p ref/hg38_genome_index -s reads_1.fa -t reads_vs_genome_1.arf  
 
 # 5. Download miRBase reference sequences for known miRNAs
 # For quantifying known miRNAs, you need the mature and hairpin sequences.
@@ -25,6 +16,4 @@ awk '{print $1}' hsa.hairpin.fa> one_column_hsa.hairpin.fa
 fastaparse.pl one_column_hsa.mature.fa -b > one_column_hsa.mature.clean.fa
 fastaparse.pl one_column_hsa.hairpin.fa -b > one_column_hsa.hairpin.clean.fa
 
-###then the quantifier to get the raw counts read 
- quantifier.pl -m ref/one_column_hsa.mature.clean.fa -p ref/one_column_hsa.hairpin.clean.fa -r reads_1.fa -t hsa -c config.txt  
 
