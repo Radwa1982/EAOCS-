86a502fad4b437b1a4d3ef57a2e4ca8aabaae318
Radwa1982 <radwa_noreen@hotmail.co.uk>
2025-09-12 21:01:42 +0100

    Update referencing.sh

--- miRNA/referencing.sh
+++ miRNA/referencing.sh
@@ -1,10 +1,10 @@
 # refrencing 
- wget https://ftp.ensembl.org/pub/release-115/fasta/homo_sapiens/ncrna/Homo_sapiens.GRCh38.ncrna.fa.gz   
+   
 
 #Indexing using bowtie
 module avail
 module load
- bowtie-build Homo_sapiens.GRCh38.ncrna.fa.gz hg38_ncrna_index 
+ bowtie-build 
 # The mapping step 
 mapper.pl config.txt -d -j -h -e -l 18 -m -p ref/hg38_genome_index -s reads_1.fa -t reads_vs_genome_1.arf  
 
